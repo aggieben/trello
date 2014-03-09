3d74bf01f6bbad57e04972c1f20ea8b37bb2f22b
Benjamin Collins <ben.collins@acm.org>
2014-03-08 23:43:59 -0600

    Trello constructor

--- trello.go
+++ trello.go
@@ -7,7 +7,22 @@ package trello
 
 import ()
 
+// Context is a type used to pass global, semi-const parameters to API
+// requests, like access tokens, application secret, current user, etc.
+type context struct {
+	Key    string
+	Secret string
+	Token  string
+}
+
 // Trello is the main type that users of the SDK will interact with.  It
 // encapsulates an HTTP client and manages state.
 type Trello struct {
+	context *context
+}
+
+// NewTrello initializes a new Trello object with the provided appKey,
+// appSecret, and userToken
+func NewTrello(appKey string, appSecret string, userToken string) *Trello {
+	return &Trello{&context{appKey, appSecret, userToken}}
 }
