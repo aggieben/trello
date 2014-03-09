451a0fbdd15ee49a7038b20585fc73cc8b4ec329
Benjamin Collins <ben.collins@acm.org>
2014-03-08 22:58:29 -0600

    api client

---
+++ trello.go
@@ -0,0 +1,13 @@
+// Copyright © Ben Collins <ben.collins@acm.org>
+// See LICENSE for license.
+
+// Package trello implements a Go-language SDK for the Trello
+// (https://trello.com/docs/index.html)
+package trello
+
+import ()
+
+// Trello is the main type that users of the SDK will interact with.  It
+// encapsulates an HTTP client and manages state.
+type Trello struct {
+}
