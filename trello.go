c33f083dfa948da012ff170bee0644db938befc0
Benjamin Collins <ben.collins@acm.org>
2014-03-10 00:11:14 -0500

    flesh out the approach a little more: embedding model types into the main Trello type.

--- trello.go
+++ trello.go
@@ -5,24 +5,47 @@
 // (https://trello.com/docs/index.html)
 package trello
 
-import ()
+import (
+	"net/http"
+)
+
+func Version() string {
+	return "0.1"
+}
 
 // Context is a type used to pass global, semi-const parameters to API
 // requests, like access tokens, application secret, current user, etc.
 type context struct {
-	Key    string
-	Secret string
-	Token  string
+	Version string
+	Key     string
+	Secret  string
+	Token   string
+	client  *http.Client
 }
 
 // Trello is the main type that users of the SDK will interact with.  It
 // encapsulates an HTTP client and manages state.
 type Trello struct {
 	context *context
+
+	Member
 }
 
-// NewTrello initializes a new Trello object with the provided appKey,
-// appSecret, and userToken
-func NewTrello(appKey string, appSecret string, userToken string) *Trello {
-	return &Trello{&context{appKey, appSecret, userToken}}
+// TrelloParams provides initialization parameters to the trello client.
+type TrelloParams struct {
+	Version   string
+	AppKey    string
+	AppSecret string
+	UserToken string
+}
+
+// NewTrello initializes a new Trello object using the provided parameters.
+func NewTrello(params TrelloParams) *Trello {
+	if params.Version == "" {
+		params.Version = "1"
+	}
+
+	return &Trello{
+		context: &context{params.Version, params.AppKey, params.AppSecret, params.UserToken, &http.Client{}},
+	}
 }
